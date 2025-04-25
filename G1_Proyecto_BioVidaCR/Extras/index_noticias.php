4e6a74c98e1aff63d002a9a4582d5e1a879d45b9
APaolaMM <101841696+APaolaMM@users.noreply.github.com>
2025-04-24 23:16:40 -0600

    Update index_noticias.php
    Agregué API de noticias que filtra por país, idioma y categoría

--- G1_Proyecto_BioVidaCR/Extras/index_noticias.php
+++ G1_Proyecto_BioVidaCR/Extras/index_noticias.php
@@ -2,36 +2,37 @@
 
 <div class="container mt-4">
   <div class="row">
-    <div class="col-md-4 mb-4">
-      <div class="card h-100">
-        <img src="https://www.larepublica.net/storage/images/2016/10/31/201610311559440.620-PORTADA-CMYK-GERSON.jpg" class="card-img-top" alt="Gandoca-Manzanillo">
-        <div class="card-body">
-          <h5 class="card-title">Costa Rica cierra sus dos zoológicos estatales</h5>
-          <p class="card-text">Costa Rica ha decidido cerrar sus dos zoológicos estatales para promover la conservación de la vida silvestre en su hábitat natural.</p>
-          <a href="https://www.dw.com/es/costa-rica-cierra-sus-dos-zool%C3%B3gicos-estatales/a-69055441" class="btn btn-success" target="_blank">Ver</a>
+    <?php
+    $queryString = http_build_query([
+      'api_token' => 'dBSympb1N5lZRsaAJMCvUbjTzf9tafSIP9znU4dv',
+      'search' => 'Costa Rica + (naturaleza | animal | conservacion | biodiversidad)',
+      'language' => 'es',
+    ]);
+
+    $ch = curl_init(sprintf('%s?%s', 'https://api.thenewsapi.com/v1/news/all', $queryString));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $json = curl_exec($ch);
+
+    curl_close($ch);
+
+    $apiResult = json_decode($json, true);
+    
+    foreach ($apiResult['data'] as $item) {
+      echo "
+      <div class='col-md-4 mb-4'>
+        <div class='card h-100'>
+          <img src='{$item['image_url']}' class='card-img-top' alt='Gandoca-Manzanillo'>
+          <div class='card-body'>
+            <h5 class='card-title'>{$item['title']}</h5>
+            <p class='card-text'>{$item['description']}</p>
+            <a href='{$item['url']}' class='btn btn-success' target='_blank'>Ver</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-md-4 mb-4">
-      <div class="card h-100">
-        <img src="https://www.gandocatours.com/wp-content/uploads/2019/04/manzanilloCaribbeanBeachesHeader.jpg" class="card-img-top" alt="Gandoca-Manzanillo">
-        <div class="card-body">
-          <h5 class="card-title">Área total que se debe recuperar de Gandoca-Manzanillo</h5>
-          <p class="card-text">El área de Gandoca-Manzanillo requiere la recuperación de 231 hectáreas para preservar su biodiversidad única.</p>
-          <a href="https://ameliarueda.com/noticia/area-recuperar-gandoca-manzanillo-231-hectareas-sinac-noticias-costa-rica" class="btn btn-success" target="_blank">Ver</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4 mb-4">
-      <div class="card h-100">
-        <img src="https://static.nationalgeographicla.com/files/styles/image_3200/public/01-sloth-energy-nationalgeographic_1046424.jpg?w=1900&h=1267" class="card-img-top" alt="Perezoso de Costa Rica">
-        <div class="card-body">
-          <h5 class="card-title">El perezoso, un animal icónico de Costa Rica</h5>
-          <p class="card-text">El perezoso es uno de los animales más emblemáticos de Costa Rica, conocido por su estilo de vida tranquilo y su importancia ecológica.</p>
-          <a href="https://www.inoutviajes.com/noticia/27362/turismo-internacional/el-perezoso-un-animal-iconico-de-costa-rica.html" class="btn btn-success" target="_blank">Ver</a>
-        </div>
-      </div>
-    </div>
+      ";
+    }
+    ?>
   </div>
 </div>
 
