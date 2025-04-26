ed1e144ce68bd38d8269ba902fc28f04ef623591
APaolaMM <101841696+APaolaMM@users.noreply.github.com>
2025-04-25 20:25:34 -0600

    Update conexion_db.php

--- G1_Proyecto_BioVidaCR/conexion_db.php
+++ G1_Proyecto_BioVidaCR/conexion_db.php
@@ -7,6 +7,7 @@ $dbname  = 'biovidacr';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $contrasena);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new mysqli($host, $usuario, $contrasena, $dbname);
 } catch (PDOException $e) {
     die("Error de conexión a la base de datos: " . $e->getMessage());
 }
