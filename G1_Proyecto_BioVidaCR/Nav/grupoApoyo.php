36b1758685b3f8559afc24eb7874f2ddce078e61
APaolaMM <101841696+APaolaMM@users.noreply.github.com>
2025-04-25 20:15:50 -0600

    Update grupoApoyo.php

--- G1_Proyecto_BioVidaCR/Nav/grupoApoyo.php
+++ G1_Proyecto_BioVidaCR/Nav/grupoApoyo.php
@@ -1,5 +1,7 @@
+<?php require_once '../conexion_db.php'; ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,107 +13,93 @@
             background-color: #f0f8ff !important;
             border-bottom: 1px solid #d1c4e9;
         }
+
         .card {
             border-radius: 15px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             margin-bottom: 20px;
         }
+
         .card-img-top {
             height: 200px;
             object-fit: cover;
             border-top-left-radius: 15px;
             border-top-right-radius: 15px;
         }
+
         .card-body {
             padding: 20px;
         }
+
         .card-title {
             font-size: 1.25rem;
             margin-bottom: 10px;
         }
+
         .footer {
             background-color: #343a40;
             padding: 2rem 0;
             color: #ccc;
             margin-top: 5rem;
         }
+
         .footer p {
             margin-bottom: 0px;
         }
     </style>
 </head>
+
 <body>
 
     <nav class="navbar navbar-expand-lg navbar-light">
-    <a class="navbar-brand" href="/G1_Proyecto_BioVidaCR/index.php">BioVidaCR</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="/G1_Proyecto_BioVidaCR/index.php">BioVidaCR</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/G1_Proyecto_BioVidaCR/index.php?page=inicioSesion">Inicio Sesion</a>
-               </li>
-            <li class="nav-item">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page"
+                        href="/G1_Proyecto_BioVidaCR/index.php?page=inicioSesion">Inicio Sesion</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/reportarEmergencia.php">Reportar Emergencia</a>
                 </li>
                 <li class="nav-item">
-                        <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/registroMascota.php">Registro de Mascotas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/catalogo.php">Catalogo de Animales</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/contactoAyuda.php">Contactos de Ayuda</a>
-                    </li>
+                    <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/registroMascota.php">Registro de Mascotas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/catalogo.php">Catalogo de Animales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/G1_Proyecto_BioVidaCR/Nav/contactoAyuda.php">Contactos de Ayuda</a>
+                </li>
             </ul>
         </div>
     </nav>
     <div class="text-center mb-4">
-                    <h2>Grupos de Apoyo</h2>
-                </div>
+        <h2>Grupos de Apoyo</h2>
+    </div>
     <main class="container mt-5">
         <div class="row">
-            <div class="col-md-6">
-                <div class="card">
-                    <img src="https://destinosviajeros.com/wp-content/uploads/formidable/36/ccsaP1030885-1.jpg" class="card-img-top" alt="Centro de Conservación Santa Ana">
-                    <div class="card-body">
-                        <h5 class="card-title">Centro de Conservación Santa Ana</h5>
-                        <p>Fundazoo se dedica a la investigación, la reproducción en cautiverio de especies de flora y fauna tropicales, educación ambiental, conservación y rehabilitación de hábitat y a la recreación ecoturística.</p>
-                        <a href="https://www.facebook.com/CentrodeConservacion/?locale=es_LA" class="btn btn-primary">Visitar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <img src="https://rescatewildlife.org/wp-content/uploads/2023/09/Group-1086-2-1.webp" class="card-img-top" alt="Rescate Wildlife Rescue Center">
-                    <div class="card-body">
-                        <h5 class="card-title">Rescate Wildlife Rescue Center</h5>
-                        <p>Este centro se dedica al rescate y rehabilitación de diversas especies de animales silvestres.</p>
-                        <a href="https://rescatewildlife.org/es/visitenos/?gad_source=1&gbraid=0AAAAAqu_r2JA1YsAocSs-AegBVvsV0-zo&gclid=Cj0KCQjw5azABhD1ARIsAA0WFUF9l-zjMYZb6O4VTeq1i9QX3omwwk7LK4kRDups_PQ4FPFTRZEWrocaAib5EALw_wcB" class="btn btn-primary">Visitar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <img src="https://scontent.fsyq8-1.fna.fbcdn.net/v/t39.30808-6/327296856_1333094940591421_2685742748681785215_n.png?_nc_cat=106&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=aNYHs_F1EN4Q7kNvwG2A6_T&_nc_oc=AdnIAN3ze-hEtM93UaubmhuKcp72ykkbU9z75_5-uv0u-vN7S2mx_A3SlNPsjeZboCE&_nc_zt=23&_nc_ht=scontent.fsyq8-1.fna&_nc_gid=3I5s7fJvXtpUqRO02S3mvQ&oh=00_AfHycV96by6bbXWIiyoAX1Qo9HXmDX29xT1UBso93gL3yg&oe=6811E17B" class="card-img-top" alt="Zoológico Simón Bolívar">
-                    <div class="card-body">
-                        <h5 class="card-title">Zoológico Simón Bolívar</h5>
-                        <p>Fundazoo se dedica a la investigación, la reproducción en cautiverio de especies de flora y fauna tropicales, educación ambiental, conservación y rehabilitación de hábitat y a la recreación ecoturística.</p>
-                        <a href="https://www.facebook.com/zoosimonbolivar/?locale=es_LA" class="btn btn-primary">Visitar</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <img src="https://ponderosacostarica.com/wp-content/uploads/2023/07/Whatsapp-Ponderosa-Costa-Rica.webp" class="card-img-top" alt="Ponderosa Adventure Park">
-                    <div class="card-body">
-                        <h5 class="card-title">Ponderosa Adventure Park</h5>
-                        <p>Parque de aventura con animales salvajes y diversas actividades al aire libre.</p>
-                        <a href="https://ponderosacostarica.com/" class="btn btn-primary">Visitar</a>
-                    </div>
-                </div>
-            </div>
+        <?php
+        global $conn;
+        $result = $conn->query("SELECT * FROM grupos_ayuda");
+        while ($row = $result->fetch_assoc()) {
+            echo "<div class='col-md-6'>";
+            echo "<div class='card'>";
+            echo "<img src='{$row['imagen']}' alt='{$row['nombre_grupo']}' class='card-img-top'>";
+            echo "<div class='card-body'>";
+            echo "<h5 class='card-title'>{$row['nombre_grupo']}</h5>";
+            echo "<p>{$row['descripcion_grupo']}</p>";
+            echo "<a href='{$row['sitio_web']}' class='btn btn-primary'>Visitar</a>";
+            echo "</div>";
+            echo "</div>";
+            echo "</div>";
+        }
+        $conn->close();
+        ?>
         </div>
     </main>
 
@@ -124,4 +112,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="../../js/scripts.js"></script>
 </body>
+
 </html>
